500962994362b038e034b09c73af592e38714d7d
Christofer <cristo2014f@outllok.es>
2015-05-27 21:00:03 -0500

    Search Update

--- Codificador/main.cpp
+++ Codificador/main.cpp
@@ -2,6 +2,8 @@
 #include <cstdlib>
 #include <time.h>
 #include <windows.h>
+#include <vector>
+
 
 using namespace std;
 
@@ -142,6 +144,10 @@ void gg(){
     system("C:\\Eclipses\\eclipselunaEE\\eclipse.exe");
 }
 
+void yugi(){
+    WinExec("D:\\Yu-Gi-Oh!MarikTheDarkness\\marik_pc.exe",SW_SHOW);
+}
+
 void search(){
     string sentence;
     string resultado;
@@ -160,21 +166,51 @@ void search(){
         }
     }
     string temp = "start https://www.google.com.pe/#q=";
-    resultado.insert(resultado.end(),' ');
-    resultado.insert(resultado.end(),' ');
-    resultado.insert(resultado.end(),' ');
-    char google[36 + resultado.length()] ;
-    for(int i= 0; i < 35; i++){
-        google[i] = temp[i];
+    char google[36 + resultado.length()];
+    for(int i = 0; i< 3; i++){
+        google[i] = ' ';
+    }
+    for(int i= 1; i < 35 + 1; i++){
+        google[i - 1] = temp[i - 1];
+        if(i % 4 == 0){
+            google[i] = ' ';
+            google[i +1] = ' ';
+            google[i +2] = ' ';
+        }
     }
     string temp2 = "start https://www.bing.com/search?q=";
     char bing[37 +  resultado.length()];
-    for(int i = 0; i < 36; i++){
-        bing[i] = temp2[i];
+    for(int i = 0; i< 3; i++){
+        bing[i] = ' ';
     }
+    for(int i = 1; i < 36 + 1; i++){
+        bing[i - 1] = temp2[i - 1];
+        if(i % 4 == 0){
+            bing[i] = ' ';
+            bing[i +1] = ' ';
+            bing[i +2] = ' ';
+        }
+    }
+    int n = 3;
     for(int i = 0; i < resultado.length(); i++){
         google[i + 35] = resultado[i];
         bing[i + 36] = resultado[i];
+        if(i == 0){
+            google[36] = ' ';
+            google[37] = ' ';
+            google[38] = ' ';
+        }
+        else if(i % 4 == 0){
+            google[i + 36] = ' ';
+            google[i + 37] = ' ';
+            google[i + 38] = ' ';
+        }
+        else if(i == n){
+            bing[i + 37] = ' ';
+            bing[i + 38] = ' ';
+            bing[i + 39] = ' ';
+            n = n + 4;
+        }
     }
     system(google);
     system(bing);
@@ -204,9 +240,103 @@ string clave(){
     return s;
 }
 
+void salir(){
+    exit(0);
+}
+void cod(){
+    string comando2;
+    cin>>comando2;
+    if(comando2 == "-suf"){
+        codificador_suf();
+    }
+}
+void dec(){
+    string comando2;
+    cin>>comando2;
+    if(comando2 == "-suf"){
+        decodificador_suf();
+    }
+}
+void limpiar(){
+    system("cls");
+}
+void init(){
+    string comando2;
+    cin>>comando2;
+    if(comando2 == "-social"){
+        system("start https://www.facebook.com");
+        system("start https://twitter.com");
+        sucess();
+    }
+    if(comando2 == "-candy"){
+        system("start https://apps.facebook.com/candycrush/");
+        sucess();
+    }
+    if(comando2 == "-alka"){
+        system("start http://www.twitch.tv/mym_alkapone");
+        system("start https://www.youtube.com/channel/UCLq35g_MyAFUqd1xdkxN5kg");
+        sucess();
+    }
+}
+void busqueda(){
+    search();
+    sucess();
+}
+void testeo(){
+    test();
+    sucess();
+}
+void noexiste(){
+}
+
+vector<string> listaComandos(){
+    vector<string> listaComandos;
+    listaComandos.push_back("no existe@@");
+    listaComandos.push_back("/exit");
+    listaComandos.push_back("/cod");
+    listaComandos.push_back("/dec");
+    listaComandos.push_back("/init");
+    listaComandos.push_back("/search");
+    listaComandos.push_back("/test");
+    listaComandos.push_back("/clear");
+    return listaComandos;
+}
+
+vector<void (*)()> listaFunciones(){
+    vector<void (*)()> funciones;
+    void (*temp)() = noexiste;
+    funciones.push_back(temp);
+    temp = salir;
+    funciones.push_back(temp);
+    temp = cod;
+    funciones.push_back(temp);
+    temp = dec;
+    funciones.push_back(temp);
+    temp = init;
+    funciones.push_back(temp);
+    temp = busqueda;
+    funciones.push_back(temp);
+    temp = testeo;
+    funciones.push_back(temp);
+    temp = limpiar;
+    funciones.push_back(temp);
+    return funciones;
+}
+
+int findComando(vector<string> &comandos, string comando){
+    for(int i = 1; i < comandos.size() +1; i++){
+        if(comandos[i] == comando){
+            return i;
+        }
+    }
+    return 0;
+
+}
 
 int main()
 {
+    vector<string> comandos = listaComandos();
+    vector<void (*)()> funciones = listaFunciones();
     srand (time(NULL));
     string comando1, comando2,usuario,pass;
     cout<<"->WELCOME"<<endl;
@@ -216,62 +346,16 @@ int main()
     pass = clave();
     cout<<endl;
     if(usuario == "WonderMel" and pass == "Cristo2014"){
-        cout<<"WONDERMEL>>";
-        cin>>comando1;
-        while(comando1 != "/exit"){
-            if(comando1 == "/cod"){
-                cin>>comando2;
-                if(comando2 == "-suf"){
-                    codificador_suf();
-                }
-            }
-            else if(comando1 == "/dec"){
-                cin>>comando2;
-                if(comando2 == "-suf"){
-                    decodificador_suf();
-                }
-            }
-            else if(comando1 == "/clear"){
-                system("cls");
-            }
-            else if(comando1 == "/init"){
-                cin>>comando2;
-                if(comando2 == "-social"){
-                    system("start https://www.facebook.com");
-                    system("start https://twitter.com");
-                    sucess();
-                }
-                if(comando2 == "-candy"){
-                    system("start https://apps.facebook.com/candycrush/");
-                    sucess();
-                }
-                if(comando2 == "-alka"){
-                    system("start http://www.twitch.tv/mym_alkapone");
-                    system("start https://www.youtube.com/channel/UCLq35g_MyAFUqd1xdkxN5kg");
-                    sucess();
-                }
-            }
-            else if(comando1 == "/search"){
-                search();
-                sucess();
-            }
-            else if(comando1 == "/gg"){
-                gg();
-            }
-            else if(comando1 == "/test"){
-                test();
-                sucess();
-            }
-            else{
-                cout<<"->NO SE ENCUENTRA EL COMANDO '"<<comando1<<"'"<<endl;
-            }
+        while(1){
             cout<<"WONDERMEL>>";
             cin>>comando1;
+            int num = findComando(comandos,comando1);
+            void (*temp)() =funciones[num];
+            temp();
         }
     }
     else{
         cout<<"->INCORRECTO";
         exit(0);
     }
-    exit(0);
 }
